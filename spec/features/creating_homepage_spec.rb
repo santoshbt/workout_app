c0b57b9ed7aa067e6731aa8051333ef62fde0b35
Santosh T <bt.santosh01@gmail.com>
2017-02-06 16:45:48 +0000

    Implement Home page

--- spec/features/creating_homepage_spec.rb
+++ spec/features/creating_homepage_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature 'Creating homepage' do
         
         expect(page).to have_link('Home')
         expect(page).to have_link('Athletes Den')
-        expect(page).to have_link('Workout Lounge!')
-        expect(page).to have_link('Show off your workout')
+        expect(page).to have_content('Workout Lounge!')
+        expect(page).to have_content('Show off your workout')
     end
 end
