e025f780872d7f6ed6fcf26ef8e5dc1cc2f24a45
Santosh T <bt.santosh01@gmail.com>
2017-02-06 14:13:46 +0000

    create homepage spec

---
+++ spec/features/creating_homepage_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.feature 'Creating homepage' do
+    visit '/'
+    
+    expect(page).to have_link('Home')
+    expect(page).to have_link('Athletes Den')
+    expect(page).to have_link('Workout Lounge!')
+    expect(page).to have_link('Show off your workout')
+end
