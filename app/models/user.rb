a53fdd90a5559d4f64999c8dac950d69e3ecf0eb
Santosh T <bt.santosh01@gmail.com>
2017-02-11 17:25:28 +0000

    Implement searching for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,18 @@ class User < ApplicationRecord
   end
   
   self.per_page = 10
+  
+  def self.search_by_name(name)
+    names_array = name.split(' ')
+
+    if names_array.size == 1
+      where('first_name LIKE ? or last_name LIKE ?',
+        "%#{names_array[0]}%", "%#{names_array[0]}%").order(:first_name)
+    else
+      where('first_name LIKE ? or first_name LIKE ? or last_name LIKE ?
+        or last_name LIKE ?', "%#{names_array[0]}%",
+        "%#{names_array[1]}%", "%#{names_array[0]}%",
+        "%#{names_array[1]}%").order(:first_name)
+    end
+  end
 end
