56d014f4c9ba6519b7d699af97e1e2f65737a3d8
Santosh T <bt.santosh01@gmail.com>
2017-02-11 09:57:56 +0000

    display no workouts

--- spec/features/exercises/listing_exercises_spec.rb
+++ spec/features/exercises/listing_exercises_spec.rb
@@ -27,4 +27,13 @@ RSpec.feature 'Listing Exercises' do
         expect(page).to have_content(@e2.workout)
         expect(page).to have_content(@e2.workout_date)
     end
+    
+    scenario 'shows no exercises if none created' do
+        @john.exercises.delete_all
+        
+        visit '/'
+        click_link 'My Lounge'
+        
+        expect(page).to have_content('No Workouts yet')
+    end
 end
