5e95f9c0657d08b8f33313ee069796079768b7d7
Santosh T <bt.santosh01@gmail.com>
2017-02-11 06:56:30 +0000

    add routes, index and new action to exercises controller

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature "Creating Exercise" do
     before do 
         @john = User.create!(email: 'john@example.com', password: 'password')
-        loging_as(@john)
+        login_as(@john)
     end
     
     scenario "with valid inputs" do
