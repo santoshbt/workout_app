e96e5e51d01cf4052d0061c4e6abf19627be7dbf
Santosh T <bt.santosh01@gmail.com>
2017-02-11 15:18:05 +0000

    add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
          
   has_many :exercises
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   
   def full_name
     [first_name, last_name] * (" ")
