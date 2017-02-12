f888ea8a0eae7d90b952eec9aeac41d3f7dc1004
Santosh T <bt.santosh01@gmail.com>
2017-02-12 07:34:08 +0000

    modify lounge layout

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -6,8 +6,8 @@ class ExercisesController < ApplicationController
         @friends = current_user.friends
         set_current_room
         @message = Message.new
-        @messages = current_room.messages if current_room
-        @followers = Friendship.where('friend_id = ?', current_user.id)
+        @messages = current_room.messages.reverse 
+        @followers = Friendship.where(friend_id: current_user.id)
     end
     
     def show
