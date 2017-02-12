9e9bb9fd4ffc9c7ad0a46721b510f1633b89884a
Santosh T <bt.santosh01@gmail.com>
2017-02-12 06:44:33 +0000

    Implement messages controller

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -4,6 +4,10 @@ class ExercisesController < ApplicationController
     def index
         @exercises = current_user.exercises
         @friends = current_user.friends
+        set_current_room
+        @message = Message.new
+        @messages = current_room.messages if current_room
+        @followers = Friendship.where('friend_id = ?', current_user.id)
     end
     
     def show
@@ -51,5 +55,14 @@ class ExercisesController < ApplicationController
     
     def set_exercise
         @exercise = current_user.exercises.find(params[:id])
+    end
+    
+    def set_current_room
+        if(params[:room_id])
+            @room = Room.find_by(id: params[:room_id])
+        else
+            @room = current_user.room
+        end
+        session[:current_room] = @room.id if @room
     end
 end
